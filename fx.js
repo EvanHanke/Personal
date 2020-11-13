60a2a91d5442b422b2e8c82a6201e266493f3556
EvanHanke <33070738+EvanHanke@users.noreply.github.com>
2020-11-12 23:46:23 -0500

    dynamic list

--- fx.js
+++ fx.js
@@ -1,7 +1,7 @@
 //option class with animation methods
 class Option{
 
-  constructor(div){
+  constructor(div, desc){
     this.div = div;
     this.div.onmouseenter = this.hover.bind(this);
     this.div.onmouseleave = this.unhover.bind(this);
@@ -9,15 +9,20 @@ class Option{
     this.hovered = false;
     this.timer = 0;
     this.anim = null;
+    this.desc = desc;
   }
 
   hover(){
     selected = this;
+    contentdiv.textContent = this.desc;
     this.hovered = true;
     this.anim = setInterval(this.myanimate.bind(this), 10);
   }
   unhover(){
-    if(this == selected) selected = null;
+    if(this == selected){
+      contentdiv.textContent = descs[1];
+      selected = null;
+    }
     this.hovered = false;
   }
 
@@ -41,39 +46,33 @@ class Option{
 var content = [];
 
 //main
-var option_divs = [];
+var option_div = null;
 var options = []; //array of option objects on the website
 var contentdiv = document.getElementById("content");
-var txtfile;
-var selected;
+var descs;
+var selected = null;
 
 $.get("https://evanhanke.github.io/Personal/descriptions.txt",
     function(data) {
-    console.log(data);
+    descs = String(data).split("#");
+    contentdiv.innerHTML = descs[1];
+    console.log(descs);
+    init();
 });
 
-document.body.onload = function(){
+function init(){
 
-
-  option_divs = document.getElementsByClassName("option");
-  for(i = 0; i < option_divs.length; i++){
-    options.push(new Option(option_divs[i]));
+  option_div = document.getElementsByClassName("option")[0];
+  options.push(option_div, descs[1]);
+  for(i = 1; i < descs.length; i++){
+    var clone = option_div.cloneNode( true );
+    clone.innerText = descs[i+2].split("\n")[1];
+    document.getElementById('sidebar').appendChild(clone );
+    options.push(new Option(clone, descs[i+2]));
   }
 
 }
 
-function readTextFile(file){
-    var request = new XMLHttpRequest();
-    request.open("GET", file);
-    console.log(txtfile);
-    request.onreadystatechange = function() {
-    if (request.readyState === 0) {
-        contentdiv.innterHTML = "loaded";
-        console.log(txtfile);
-    }
-  }
-  request.send();
-}
 
 var image = document.getElementById("back-image");
 onmousemove = function(e){
